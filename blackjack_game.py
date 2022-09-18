62b189cbc08287e0be49528b4c65c9b95d7a6901
Pranav Gautam <95767267+pranavgautam29@users.noreply.github.com>
2022-09-18 21:58:12 +0530

    Project #10/100
    Blackjack game also known as 21

---
+++ blackjack_game.py
@@ -0,0 +1,45 @@
+import random
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+player_cards = [random.choice(cards) , random.choice(cards)]
+computer_cards = [random.choice(cards)]
+play_again = True
+while play_again:
+    print("Your cards:",player_cards)
+    print("Computers cards:",computer_cards)
+    player_choice = input("Type 'y' to get another card or 'n' to pass:") 
+    if player_choice == 'y':
+        player_cards.append(random.choice(cards))
+        computer_cards.append(random.choice(cards))
+    elif player_choice == 'n':
+        computer_cards.append(random.choice(cards))
+    else:
+        print("Invalid Input!!")
+
+    def sum_cards(cards):
+        sum = 0
+        for i in cards :
+            sum += i
+        return sum
+
+    player_sum = sum_cards(player_cards)
+    computer_sum = sum_cards(computer_cards)
+    print("Your final hand:",player_cards)
+    print("Computer's final hand:",computer_cards)
+    if player_sum > 21:
+        print("Computer Won!")
+    elif computer_sum > 21:
+        print("You Won!!")
+    elif computer_sum == player_sum:
+        print("Its a Draw")
+    else:
+        if player_sum > computer_sum:
+            print("You Won!!")
+        elif player_sum < computer_sum:
+            print("Computer Won!!")
+    if input("Do you want to play again(y or n):") == 'n':
+        print("GoodBye!!")
+        play_again = False
+
+
+
+
